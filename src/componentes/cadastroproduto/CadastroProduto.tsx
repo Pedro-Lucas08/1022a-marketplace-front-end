41c70b3189ae5fd05a92ef9faf7f0bd71c7bcece
Bladekkkkkk <pedro.silva26@estudante.ifms.edu.br>
2025-02-03 15:17:41 -0400

    Update CadastroProduto.tsx

--- src/componentes/cadastroproduto/CadastroProduto.tsx
+++ src/componentes/cadastroproduto/CadastroProduto.tsx
@@ -2,10 +2,11 @@ import {  ChangeEvent, FormEvent, useState } from "react"
 import { useNavigate } from 'react-router-dom';
 function CadastroProduto(){
     const navigate = useNavigate()
-    const [id,setId] = useState("")
     const [nome,setNome] = useState("")
-    const [descricao,setDescricao] = useState("")
+    const [tamanho,setTamanho] = useState("")
     const [preco,setPreco] = useState("")
+    const [marca,setMarca] = useState("")
+    const [modelo,setModelo] = useState("")
     const [imagem,setImagem] = useState("")
     async function handleForm(event:FormEvent){
         event.preventDefault()
@@ -16,9 +17,10 @@ function CadastroProduto(){
                     "Content-Type":"application/json"
                 },
                 body:JSON.stringify({
-                    id:id,
                     nome:nome,
-                    descricao:descricao,
+                    tamanho:tamanho,
+                    marca:marca,
+                    modelo:modelo,
                     preco:preco,
                     imagem:imagem
                 })
@@ -37,14 +39,17 @@ function CadastroProduto(){
         }
         
     }
-    function handleId(event:ChangeEvent<HTMLInputElement>){
-        setId(event.target.value)
-    }
     function handleNome(event:ChangeEvent<HTMLInputElement>){
         setNome(event.target.value)
     }
-    function handleDescricao(event:ChangeEvent<HTMLInputElement>){
-        setDescricao(event.target.value)
+    function handleTamanho(event:ChangeEvent<HTMLInputElement>){
+        setTamanho(event.target.value)
+    }
+    function handleMarca(event:ChangeEvent<HTMLInputElement>){
+        setMarca(event.target.value)
+    }
+    function handleModelo(event:ChangeEvent<HTMLInputElement>){
+        setModelo(event.target.value)
     }
     function handlePreco(event:ChangeEvent<HTMLInputElement>){
         setPreco(event.target.value)
@@ -56,20 +61,30 @@ function CadastroProduto(){
         <>
             <h1>Meu Componente de Cadastro de Produtos</h1>
             <form onSubmit={handleForm}>
-                <div>
-                    <input placeholder="Id" type="text" name="id" id="id" onChange={handleId} />
+            <div>
+                    <label htmlFor="nome">Nome</label>
+                    <input placeholder="Nome" type="text" name="nome" id="nome" value={nome} onChange={handleNome} />
                 </div>
                 <div>
-                    <input placeholder="Nome" type="text" name="nome" id="nome" onChange={handleNome} />
+                    <label htmlFor="tamanho">Descrição</label>
+                    <input placeholder="Tamanho" type="text" name="tamanho" id="tamanho" value={tamanho} onChange={handleTamanho} />
                 </div>
                 <div>
-                    <input placeholder="Descrição" type="text" name="descricao" id="descricao" onChange={handleDescricao} />
+                    <label htmlFor="marca">Descrição</label>
+                    <input placeholder="Marca" type="text" name="marca" id="marca" value={marca} onChange={handleMarca} />
                 </div>
                 <div>
-                    <input placeholder="Preço" type="text" name="preco" id="preco" onChange={handlePreco} />
+                    <label htmlFor="modelo">Descrição</label>
+                    <input placeholder="Modelo" type="text" name="modelo" id="modelo" value={modelo} onChange={handleModelo} />
                 </div>
                 <div>
-                    <input placeholder="URL Imagem" type="text" name="imagem" id="imagem" onChange={handleImagem} />
+                    <label htmlFor="preco">Preço</label>
+                    <input placeholder="Preço" type="text" name="preco" id="preco" value={preco} onChange={handlePreco} />
+                </div>
+                <div>
+                    <label htmlFor="imagem">URL Imagem</label>
+                    <input placeholder="URL Imagem" type="text" name="imagem" id="imagem" value={imagem} onChange={handleImagem} />
+                    {imagem && <img className="imagem-produto-reduzida" src={imagem} alt="Imagem do Produto" />}
                 </div>
                 <input type="submit" value="Cadastrar" />
             </form>
