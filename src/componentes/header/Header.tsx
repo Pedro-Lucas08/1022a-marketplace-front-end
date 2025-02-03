47f6f1c33c93230700ae1da46db5a0556b769059
Bladekkkkkk <pedro.silva26@estudante.ifms.edu.br>
2025-02-03 15:40:27 -0400

    Atualização de header

--- src/componentes/header/Header.tsx
+++ src/componentes/header/Header.tsx
@@ -1,21 +1,26 @@
-import {Link} from 'react-router-dom';
-function Header(){
-    return(
-        <header className="site-header">
-        
-        
-                <nav className="navigation">
-                  <ul>
-                    <Link to="/">Home</Link>
-                    <Link to="/cadastro-produto">Cadastro de Produto</Link>
-                  </ul>
-                </nav>
-        
-                <div className="header-actions">
-                  <button className="login-button">Login</button>
-                </div>
-              </header>
-    )
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header className="site-header">
+      <div className="logo">
+        <h1>Marketplace</h1>
+      </div>
+      <nav className="navigation">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/cadastro-produto">Cadastro de Produto</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="header-actions">
+        <button className="login-button">Login</button>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
